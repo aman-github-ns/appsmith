fc9dd011a031c84ed0b0ed1263896aa3a3fc3f11
gautam-ns <163974226+gautam-ns@users.noreply.github.com>
2024-08-30 05:25:07 +0000

    merge form and static build folder

--- app/client/craco.build.config.js
+++ app/client/craco.build.config.js
@@ -23,7 +23,7 @@ plugins.push(
   new WorkboxPlugin.InjectManifest({
     swSrc: "./src/serviceWorker.ts",
     mode: "development",
-    swDest: "./form/pageService.js",
+    swDest: "./static/form/pageService.js",
     maximumFileSizeToCacheInBytes: 11 * 1024 * 1024,
     exclude: [
       // Don’t cache source maps and PWA manifests.
