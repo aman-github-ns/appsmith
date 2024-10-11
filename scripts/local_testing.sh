31dbb5755c5b1dafcc1bb2281807978c54e6770e
aman-github-ns <116558763+aman-github-ns@users.noreply.github.com>
2024-10-11 12:54:44 +0530

    Update local_testing.sh

--- scripts/local_testing.sh
+++ scripts/local_testing.sh
@@ -80,10 +80,10 @@ fi
 pretty_print "Starting server build ..."
 
 pushd app/server > /dev/null
-#if ! ./build.sh -DskipTests > /dev/null; then
-#  echo Server build failed >&2
-#  exit 1
-#fi
+if ! ./build.sh -DskipTests > /dev/null; then
+  echo Server build failed >&2
+  exit 1
+fi
 pretty_print "Server build successful. Starting client build ..."
 
 popd
